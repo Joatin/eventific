353bbf044c506b497a1148f59c8e549ceba31309
Joatin Granlund <granlundjoatin@icloud.com>
2018-01-18 10:45:30 +0100

    fix(core): Fixed errors with aggregate

--- packages/core/README.md
+++ packages/core/README.md
@@ -1,3 +1,5 @@
+[![npm version](https://badge.fury.io/js/%40eventific%2Fcore.svg)](https://badge.fury.io/js/%40eventific%2Fcore)
+
 # @eventific/core
 
 The central package for eventific. It contains shared objects between the other packages.
--- packages/core/src/Event.tests.ts
+++ packages/core/src/Event.tests.ts
@@ -7,10 +7,7 @@ class TestEvent extends BaseEvent<string> {
   apply() {}
 }
 
-const mock = new TestEvent();
-
 test('It should be defined', async () => {
   expect(TestEvent).toBeDefined();
-  expect(mock.name).toBeDefined();
   expect(TestEvent.Name).toBeDefined();
 });
