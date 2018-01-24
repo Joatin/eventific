2a984dab76050da4488f28b07a19a1faea026c5a
Joatin Granlund <granlundjoatin@icloud.com>
2018-01-24 23:25:11 +0100

    ci(travis): Build fixes

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -7,12 +7,14 @@ if [ $TRAVIS_BRANCH == 'develop' ]; then
     ./node_modules/.bin/lerna publish --canary --yes --npm-client npm
     echo -e "\033[0;32mFinished deploying\033[0m"
 fi
-if [ $TRAVIS_BRANCH == 'master' ]; then
+if [[ ${TRAVIS_BRANCH} == "master"] && [ !${TRAVIS_PULL_REQUEST} ]]; then
     echo -e "\033[0;32mDeploying\033[0m"
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis CI"
     git remote set-url origin https://${GH_TOKEN}@github.com/eventific/eventific.git > /dev/null 2>&1
-    git checkout -b master
-    ./node_modules/.bin/lerna publish --conventional-commits --yes --npm-client npm
+    git checkout -B master
+    ./node_modules/.bin/lerna publish --force-publish=* --conventional-commits --yes --skip-git --npm-client npm
+    git add --all
+
     echo -e "\033[0;32mFinished deploying\033[0m"
 fi
