2d83a4b7b32667aca1242e89f9f79df6e5646750
Joatin Granlund <granlundjoatin@icloud.com>
2018-02-02 15:23:19 +0100

    ci(travis): updated build script

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -3,11 +3,12 @@ set -e
 
 echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > ~/.npmrc
 if [ $TRAVIS_BRANCH == 'develop' ]; then
-    echo -e "\033[0;32mDeploying canary build\033[0m"
-    ./node_modules/.bin/lerna publish --canary --yes --npm-client npm
+  if [ $TRAVIS_PULL_REQUEST == "false" ]; then
+    echo -e "\033[0;32mDeploying beta build\033[0m"
+    ./node_modules/.bin/lerna publish --canary=beta --npm-tag=beta --yes --npm-client npm
     echo -e "\033[0;32mFinished deploying\033[0m"
-fi
-if [ $TRAVIS_BRANCH == "master" ]; then
+  fi
+elif [ $TRAVIS_BRANCH == "master" ]; then
   if [ $TRAVIS_PULL_REQUEST == "false" ]; then
     echo -e "\033[0;32mDeploying\033[0m"
     git config --global user.email "travis@travis-ci.org"
@@ -17,6 +18,12 @@ if [ $TRAVIS_BRANCH == "master" ]; then
     ./node_modules/.bin/lerna publish --conventional-commits -m "chore(release): [ci skip] publish %s" --yes --npm-client npm
     git add --all
 
+    echo -e "\033[0;32mFinished deploying\033[0m"
+  fi
+else
+  if [ $TRAVIS_PULL_REQUEST == "false" ]; then
+    echo -e "\033[0;32mDeploying alpha build\033[0m"
+    ./node_modules/.bin/lerna publish --canary=beta --npm-tag=alpha --yes --npm-client npm
     echo -e "\033[0;32mFinished deploying\033[0m"
   fi
 fi
