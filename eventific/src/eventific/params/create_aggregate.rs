6931f6b99335ba2a763413c10936dc788683b679
Joatin Granlund <granlundjoatin@gmail.com>
2020-11-30 02:55:44 +0100

    fix

--- eventific/src/eventific/params/create_aggregate.rs
+++ eventific/src/eventific/params/create_aggregate.rs
@@ -1,10 +1,22 @@
 use slog::Logger;
 use uuid::Uuid;
+use std::fmt::Debug;
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct CreateAggregateParams<D, M> {
     pub aggregate_id: Uuid,
     pub events: Vec<D>,
     pub metadata: Option<M>,
     pub logger: Option<Logger>,
+}
+
+impl<D, M> Default for CreateAggregateParams<D, M> {
+    fn default() -> Self {
+        Self {
+            aggregate_id: Uuid::nil(),
+            events: vec![],
+            metadata: None,
+            logger: None
+        }
+    }
 }
